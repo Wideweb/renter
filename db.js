5c9aa4d9a17cf98b8f56a36070a2144999b14e60
Александр <alckevich@live.com>
2018-11-26 18:48:45 +0300

    push notifications

--- db.js
+++ db.js
@@ -12,4 +12,8 @@ bounds[rt][long]: 27.73086547851563
 
 https://ak.api.onliner.by/search/apartments?price%5Bmin%5D=50&price%5Bmax%5D=300&currency=usd&rent_type%5B%5D=1_room&rent_type%5B%5D=2_rooms&bounds%5Blb%5D%5Blat%5D=53.77793497204605&bounds%5Blb%5D%5Blong%5D=27.38754272460938&bounds%5Brt%5D%5Blat%5D=54.0239066230473&bounds%5Brt%5D%5Blong%5D=27.73086547851563&page=1&_=0.9482241707756398
 
+
+PUSH NOTIFICATIONS
+https://docs.expo.io/versions/latest/guides/using-fcm
+
 */
