d214e5b49580d7fef8c056ffcb54d19f4a66ac42
Александр <alckevich@live.com>
2019-02-01 18:59:50 +0300

    'conflicts'

--- modules/app/index.js
+++ modules/app/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Notifications } from 'expo';
 import thunk from 'redux-thunk';
-<<<<<<< HEAD
 import createSagaMiddleware from 'redux-saga'
-=======
-import registerForPushNotificationsAsync from '../common/infrastructure/push-notifications';
 import AppScreen from './containers/app.screen';
->>>>>>> 5a1397563379196182fd84533bb36e01d9c573a1
 
 import reducer from './reducer';
 import sagas from './sagas'
